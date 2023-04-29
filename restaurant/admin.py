6415c846a77dc54328457eaaa74a0776896c3512
Anitta Baby <anittab92@gmail.com>
2023-04-29 15:41:12 +0000

    remove updted on from model

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -5,6 +5,6 @@ from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Booking)
 class BookingAdmin(admin.ModelAdmin):
-    list_display =('name','number_of_guests','date','time','updated_on')
+    list_display =('name','number_of_guests','date','time')
     search_fields=('name','email','date')
 
