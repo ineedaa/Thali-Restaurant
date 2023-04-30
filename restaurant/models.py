c81b6d94834531ad3a52b7a042ed324bba95d413
Anitta Baby <anittab92@gmail.com>
2023-04-30 01:34:59 +0000

    Removed phonenumberfield

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
-from phonenumber_field.modelfields import PhoneNumberField
 from django import forms
 from django.forms.widgets import DateInput,TimeInput
 from datetime import datetime,time
@@ -11,7 +10,6 @@ from datetime import datetime,time
 class Booking(models.Model):
     name = models.ForeignKey(User,on_delete=models.CASCADE,related_name="customer",null=False)
     email= models.EmailField(null= False,blank=False)
-    phone = PhoneNumberField(null=False,blank=False)
     number_of_guests= models.IntegerField()
     date =models.DateField()
     time= models.TimeField()
@@ -27,7 +25,7 @@ class Booking(models.Model):
 class BookingForm(forms.ModelForm):
     class Meta:
         model=Booking
-        fields=['phone','number_of_guests','date','time','special_request',]
+        fields=['number_of_guests','date','time','special_request',]
         widgets = {
             'date': DateInput(attrs={'type': 'date','min': datetime.today().strftime('%Y-%m-%d')}),
             'time': TimeInput(attrs={'type': 'time'})
